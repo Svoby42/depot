864b7e3bd42f48c79d86d2a2ef3557f379d204c2
Ondrej Svoboda <admin@svoby.eu>
2022-03-24 22:36:41 +0100

    product test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "price of product must be positive" do
+    product = Product.new(title: "kniha jedna", description: "kniha kniha", image_url: "kniha.jpg")
+    product.price = -1
+    assert product.invalid?
+    product.price = 0
+    assert product.invalid?
+    product.price = 1
+    assert product.valid?
+  end
+
 end

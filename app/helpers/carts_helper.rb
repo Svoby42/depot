b67f509b940356464f653c2a441ca72466cdb8fc
Ondrej Svoboda <admin@svoby.eu>
2022-03-26 01:15:00 +0100

    item quantity & fix

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -3,13 +3,20 @@ module CartsHelper
   def total_price #sums prices of all products in the basket
     total = 0
     @cart.line_items.each do |item|
-      total = total + item.product.price
+      total = total + item.product.price * item.quantity
     end
     total
   end
 
   def cart_empty?
-    return @cart.line_items.count >= 1
+    current_cart.line_items.count >= 1
+  end
+
+  def current_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    if cart.line_items.count >= 1
+      @current_cart = cart
+    end
   end
 
 end
